257da1df817331a417357cc5a1a3981553eff29b
unknown <bealsben@gmail.com>
2020-04-26 12:53:00 -0700

    added fun Module

---
+++ Floofbot/Modules/Fun.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Floofbot.Modules
+{
+    public class fun : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("8ball")]
+        public async Task eightball()
+        {
+
+        }
+    }
+}
