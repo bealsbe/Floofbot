e9305fb331c1b1435a74aa907ab2383ad7c4fc9e
iShauny <ishauny@gmail.com>
2020-05-02 14:52:38 +0100

    Added commands into a group

--- Floofbot/Modules/NicknameAlert.cs
+++ Floofbot/Modules/NicknameAlert.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Floofbot.Modules
 {
+    [Group("nicknamealert")]
     public class NicknameAlert : ModuleBase<SocketCommandContext>
     {
         private FloofDataContext _floofDB;
