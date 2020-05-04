490019090198878f76ce688fc3d5a4cd0ea0efce
iShauny <ishauny@gmail.com>
2020-05-04 21:11:49 +0100

    Added zero days handle

--- Floofbot/Modules/Utilities.cs
+++ Floofbot/Modules/Utilities.cs
@@ -111,10 +111,8 @@ namespace Floofbot
             int numberTextChannels = guild.TextChannels.Count;
             int numberVoiceChannels = guild.VoiceChannels.Count;
             int daysOld = Context.Message.CreatedAt.Subtract(guild.CreatedAt).Days;
-            string createdAt = $"Created {guild.CreatedAt.DateTime.ToShortDateString()}. " +
-                               $"That's " +
-                               ((daysOld > 1) ? $"over {daysOld} days" : $"{daysOld} day") +
-                               " ago!";
+            string daysAgo = $" That's " + ((daysOld == 0) ? "today!" : (daysOld == 1) ? $"yesterday!" : $"{daysOld} days ago!");
+            string createdAt = $"Created {guild.CreatedAt.DateTime.ToShortDateString()}." + daysAgo;
             int totalMembers = guild.MemberCount;
             int onlineUsers = guild.Users.Where(mem => mem.Status == UserStatus.Online).Count();
             int numberRoles = guild.Roles.Count;
