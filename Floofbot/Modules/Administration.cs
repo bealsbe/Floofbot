87f47be659ba1b5398b4e27f40bfaf7d561ba31c
unknown <bealsben@gmail.com>
2020-05-05 08:57:31 -0700

    fixed issue with where warnlog would try to list warnings for users with no warnings

--- Floofbot/Modules/Administration.cs
+++ Floofbot/Modules/Administration.cs
@@ -156,11 +156,18 @@ namespace Floofbot.Modules
                 .Where(u => u.UserId == badUser.Id && u.GuildId == Context.Guild.Id)
                 .OrderByDescending(x => x.DateAdded).Take(24);
 
+            if (warnings.Count() == 0)
+            {
+                await Context.Channel.SendMessageAsync($"{badUser.Username}#{badUser.Discriminator} is a good noodle. They have no warnings!");
+                return;
+            }
+
             EmbedBuilder builder = new EmbedBuilder();
             int warningCount = 0;
             builder.WithTitle($"Warnings for {badUser.Username}#{badUser.Discriminator}");
             foreach (Warning warning in warnings) {
                 builder.AddField($"**{warningCount + 1}**. {warning.DateAdded.ToString("yyyy-MM-dd")}", $"```{warning.Reason}```");
+                builder.Color = ADMIN_COLOR;
                 warningCount++;
             }
             await Context.Channel.SendMessageAsync("", false, builder.Build());
