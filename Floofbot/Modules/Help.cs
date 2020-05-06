542874151cdc008a71db9c45694a88d3172da1a6
Jasper Gray <jgray7@protonmail.com>
2020-05-06 18:17:23 -0400

    Refactor help module

--- Floofbot/Modules/Help.cs
+++ Floofbot/Modules/Help.cs
@@ -12,42 +12,49 @@ namespace Floofbot.Modules
     [Discord.Commands.Name("Help")]
     public class Help : InteractiveBase
     {
-        private readonly IServiceProvider _services;
-        private readonly CommandService _commands;
-        public Help(CommandService commands, IServiceProvider services)
+        private static readonly Color EMBED_COLOR = Color.DarkGreen;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly CommandService _commandService;
+        public Help(CommandService commandService, IServiceProvider serviceProvider)
         {
-            _services = services;
-            _commands = commands;
+            _serviceProvider = serviceProvider;
+            _commandService = commandService;
         }
 
         [Summary("Show all available commands")]
         [Command("help")]
         public async Task HelpCommand()
         {
-            List<CommandInfo> commands = _commands.Commands.ToList();
-            List<ModuleInfo> modules = _commands.Modules.ToList();
-            List<EmbedFieldBuilder> fields = new List<EmbedFieldBuilder>();
-            List<PaginatedMessage.Page> pages = new List<PaginatedMessage.Page>();
+            List<ModuleInfo> modules = _commandService.Modules.ToList();
+            var moduleCommands = new Dictionary<string, List<CommandInfo>>();
 
             foreach (ModuleInfo module in modules)
             {
-                foreach (CommandInfo command in commands)
-                {
-                    if (command.Module == module)
-                    {
-                        string aliases;
-                        if (command.Aliases != null)
-                            aliases = string.Join(", ", command.Aliases);
-                        else
-                            aliases = "None";
+                moduleCommands.Add(module.Name, new List<CommandInfo>());
+            }
+            foreach (CommandInfo command in _commandService.Commands)
+            {
+                moduleCommands[command.Module.Name].Add(command);
+            }
 
-                        fields.Add(new EmbedFieldBuilder()
-                        {
-                            Name = $"{command.Name} (aliases: {aliases})",
-                            Value = command.Summary ?? "No command description available",
-                            IsInline = false
-                        });
-                    }
+            List<EmbedFieldBuilder> fields = new List<EmbedFieldBuilder>();
+            List<PaginatedMessage.Page> pages = new List<PaginatedMessage.Page>();
+            foreach (ModuleInfo module in modules)
+            {
+                foreach (CommandInfo command in moduleCommands[module.Name])
+                {
+                    string aliases;
+                    if (command.Aliases != null)
+                        aliases = string.Join(", ", command.Aliases);
+                    else
+                        aliases = "None";
+
+                    fields.Add(new EmbedFieldBuilder()
+                    {
+                        Name = $"{command.Name} (aliases: {aliases})",
+                        Value = command.Summary ?? "No command description available",
+                        IsInline = false
+                    });
                 }
                 pages.Add(new PaginatedMessage.Page
                 {
@@ -57,73 +64,57 @@ namespace Floofbot.Modules
                 });
                 fields.Clear();
             }
-            var pager = new PaginatedMessage
-            {
-                Pages = pages,
-                Color = Color.DarkGreen,
-                Content = Context.User.Mention,
-                FooterOverride = null,
-                ThumbnailUrl = Context.Client.CurrentUser.GetAvatarUrl(),
-                Options = PaginatedAppearanceOptions.Default,
-                TimeStamp = DateTimeOffset.UtcNow
-            };
-            await PagedReplyAsync(pager, new ReactionList
-            {
-                Forward = true,
-                Backward = true,
-                Jump = true,
-                Trash = true
-            });
+
+            string message = Context.User.Mention;
+            await PostHelpPages(message, pages);
         }
 
         [Summary("Show help for a specific module")]
         [Command("help")]
-        public async Task HelpCommand([Summary("module")]string requestedModule)
+        public async Task HelpCommand([Summary("module")] string requestedModule)
         {
-            List<string> moduleNames = new List<string>();
-            List<CommandInfo> commands = _commands.Commands.ToList();
-            List<EmbedFieldBuilder> fields = new List<EmbedFieldBuilder>();
-            List<PaginatedMessage.Page> pages = new List<PaginatedMessage.Page>();
-
-            // put available module names into a list instead of their object
-            foreach (ModuleInfo module in _commands.Modules.ToList())
-                moduleNames.Add(module.Name.ToLower());
-
-            if (!moduleNames.Contains(requestedModule.ToLower())) // no such cmd
+            List<string> moduleNames = _commandService.Modules.Select(x => x.Name.ToLower()).ToList();
+            if (!moduleNames.Contains(requestedModule.ToLower()))
             {
-                await Context.Channel.SendMessageAsync("Unable to find that command");
+                await Context.Channel.SendMessageAsync($"Unable to find the '{requestedModule}' module");
                 return;
             }
 
-            foreach (CommandInfo cmd in commands)
+            IEnumerable<CommandInfo> moduleCommands = _commandService.Commands
+                .Where(command => command.Module.Name.ToLower() == requestedModule.ToLower());
+            List<EmbedFieldBuilder> fields = new List<EmbedFieldBuilder>();
+            List<PaginatedMessage.Page> pages = new List<PaginatedMessage.Page>();
+            foreach (var cmd in moduleCommands)
             {
-                if (cmd.Module.Name.ToLower() == requestedModule.ToLower())
+                foreach (ParameterInfo param in cmd.Parameters)
                 {
-                    List<ParameterInfo> parameters = cmd.Parameters.ToList(); // get all params
-
-                    foreach (ParameterInfo param in parameters)
+                    fields.Add(new EmbedFieldBuilder()
                     {
-                        fields.Add(new EmbedFieldBuilder()
-                        {
-                            Name = $"{param.Name}",
-                            Value = param.Summary ?? "No parameter description available",
-                            IsInline = false
-                        });
-                    }
-                    pages.Add(new PaginatedMessage.Page
-                    {
-                        Author = new EmbedAuthorBuilder { Name = cmd.Name },
-                        Fields = new List<EmbedFieldBuilder>(fields),
-                        Description = cmd.Summary ?? "No command description available"
+                        Name = param.Name,
+                        Value = param.Summary ?? "No parameter description available",
+                        IsInline = false
                     });
-                    fields.Clear();
                 }
+                pages.Add(new PaginatedMessage.Page
+                {
+                    Author = new EmbedAuthorBuilder { Name = cmd.Name },
+                    Fields = new List<EmbedFieldBuilder>(fields),
+                    Description = cmd.Summary ?? "No command description available"
+                });
+                fields.Clear();
             }
+
+            string message = $"{Context.User.Mention} here are the commands available for {requestedModule}!";
+            await PostHelpPages(message, pages);
+        }
+
+        private async Task PostHelpPages(string message, List<PaginatedMessage.Page> pages)
+        {
             var pager = new PaginatedMessage
             {
                 Pages = pages,
-                Color = Color.DarkGreen,
-                Content = $"{Context.User.Mention} here are the commands available for {requestedModule}!",
+                Color = EMBED_COLOR,
+                Content = message,
                 FooterOverride = null,
                 ThumbnailUrl = Context.Client.CurrentUser.GetAvatarUrl(),
                 Options = PaginatedAppearanceOptions.Default,
