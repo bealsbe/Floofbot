e08dd081f40ad7f12392fcc6cbe89658f820ef4a
Jasper Gray <jgray7@protonmail.com>
2020-05-09 10:28:10 -0400

    Create new tagconfig schema

---
+++ Floofbot/Migrations/20200509154025_AddTagConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Floofbot.Migrations
+{
+    public partial class AddTagConfig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TagConfigs",
+                columns: table => new
+                {
+                    Id = table.Column<ulong>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ServerId = table.Column<ulong>(nullable: false),
+                    RestrictTagsToAdmins = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagConfigs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TagConfigs");
+        }
+    }
+}
