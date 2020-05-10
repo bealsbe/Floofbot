479e6dfac151ff29cad2aa70eeab1446599251bc
iShauny <ishauny@gmail.com>
2020-05-10 10:39:31 +0100

    Create Database Migration

---
+++ Floofbot/Migrations/20200510093729_UserAssignableRoles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Floofbot.Migrations
+{
+    public partial class UserAssignableRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAssignableRoles",
+                columns: table => new
+                {
+                    RoleId = table.Column<ulong>(nullable: false),
+                    ServerId = table.Column<ulong>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAssignableRoles", x => x.RoleId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAssignableRoles");
+        }
+    }
+}
---
+++ Floofbot/Services/Repository/Models/UserAssignableRole.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Floofbot.Services.Repository.Models
+{
+    public partial class UserAssignableRole
+    {
+        [Key]
+        public ulong RoleId { get; set; }
+        public ulong ServerId { get; set; }
+    }
+}
+
