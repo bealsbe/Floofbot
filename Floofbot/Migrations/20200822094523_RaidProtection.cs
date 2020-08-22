bd65b22fc41e0ffe2df1256b5837c8cf8109aad5
iShauny <ishauny@gmail.com>
2020-08-22 10:47:11 +0100

    Added Migration

---
+++ Floofbot/Migrations/20200822094523_RaidProtection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Floofbot.Migrations
+{
+    public partial class RaidProtection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RaidProtectionConfigs",
+                columns: table => new
+                {
+                    ServerId = table.Column<ulong>(nullable: false),
+                    Enabled = table.Column<bool>(nullable: false),
+                    ModChannelId = table.Column<ulong>(nullable: true),
+                    ModRoleId = table.Column<ulong>(nullable: true),
+                    MutedRoleId = table.Column<ulong>(nullable: true),
+                    ExceptionRoleId = table.Column<ulong>(nullable: true),
+                    BanOffenders = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RaidProtectionConfigs", x => x.ServerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RaidProtectionConfigs");
+        }
+    }
+}
