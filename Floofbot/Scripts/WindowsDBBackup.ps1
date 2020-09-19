c4f3d7774a0c4281d012c1be0b9b5d013b463d83
iShauny <ishauny@gmail.com>
2020-09-19 11:16:09 +0100

    Fixed windows backup script

--- Floofbot/Scripts/WindowsDBBackup.ps1
+++ Floofbot/Scripts/WindowsDBBackup.ps1
@@ -1,6 +1,25 @@
 ﻿$dbPath = $args[0]
-$backupPath = $args[1] + "dbBackup.db." + $args[2]
+$outputPath = $args[1]
+$maxNumberBackups = $args[2]
 
+# delete oldest backup
+$oldestBackup = $outputPath + "dbBackup.db." + $maxNumberBackups
+if (Test-Path $oldestBackup){
+    Remove-Item $oldestBackup
+}
+
+# rename old backups
+For ($i = $maxNumberBackups - 1; $i -gt 0; $i--){
+    $oldBackup = $outputPath + "dbBackup.db." + $i
+    if (Test-Path $oldBackup) # if old backup file exists
+    {
+        $newIndex = $i + 1;
+        $newFile = $outputPath + "dbBackup.db." + $newIndex
+        Rename-Item $oldBackup -NewName $newFile
+    }
+}
+
+$backupPath = $outputPath + "dbBackup.db.1"
 Copy-Item $dbPath -Destination $backupPath -force
 
 if ($?) # Copy success
