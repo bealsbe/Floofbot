023412afab10895cd03842638bb0bf95d83361fc
Joshua Chen <jkchen2@illinois.edu>
2020-09-19 06:47:43 -0500

    Update Linux backup script
    This attempts to provide maximum compatibility with most Linux environments (esp. those that don't have `bash`, such as Alpine)
    
    Still, it makes many assumptions, like the ownership of directories and the existence of the database file.

--- Floofbot/Scripts/LinuxDBBackup.sh
+++ Floofbot/Scripts/LinuxDBBackup.sh
@@ -1,7 +1,31 @@
-#!/bin/bash
+#! /bin/sh
 
-if cp $1 $2dbBackup.db.$3 ; then
-	echo "Backup Success"
+BACKUP_PREFIX="dbBackup.db."
+
+db_path=$1
+output_path=$2
+max_backups=$3
+
+partial_path="$output_path/$BACKUP_PREFIX"
+
+## Remove oldest max backup
+rm -f "$output_path/$BACKUP_PREFIX"
+
+## Shift existing backups over by one
+current_backup=`expr $max_backups - 1`
+while [ "$current_backup" -gt 0 ]; do
+    new_backup=`expr $current_backup + 1`
+    mv "$partial_path$current_backup" "$partial_path$new_backup" 2>/dev/null
+    current_backup=`expr $current_backup - 1`
+done
+
+## Copy database file
+cp $db_path "$partial_path""1"
+
+if [ $? -eq 0 ]; then
+    echo "Backup successful"
+    exit 0
 else
-	echo "Backup Fail"
+    echo "Backup failed"
+    exit 1
 fi
