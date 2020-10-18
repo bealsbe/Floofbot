9a4347dc1f7d5fba0d42ce70efbe2ab908c0a095
Shelme74 <63882440+Shelme74@users.noreply.github.com>
2020-10-18 23:33:08 +0200

    Update Conversions.cs
    Created base class

--- Floofbot/Modules/Conversions.cs
+++ Floofbot/Modules/Conversions.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+using Floofbot.Configs;
+
+namespace Floofbot
+{
+    [Summary("Conversion commands")]
+    [Discord.Commands.Name("Conversions")]
+    public class Conversions : InteractiveBase
+    {
+
+    }
+}
