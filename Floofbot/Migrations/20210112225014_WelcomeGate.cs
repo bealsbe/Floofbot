1dcdc00c7eef90474352551b1743c10e1fae03b2
iShauny <ishauny@gmail.com>
2021-01-12 22:58:24 +0000

    Created Migration

---
+++ Floofbot/Migrations/20210112225014_WelcomeGate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Floofbot.Migrations
+{
+    public partial class WelcomeGate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WelcomeGateConfigs",
+                columns: table => new
+                {
+                    GuildID = table.Column<ulong>(nullable: false),
+                    RoleId = table.Column<ulong>(nullable: true),
+                    Toggle = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WelcomeGateConfigs", x => x.GuildID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WelcomeGateConfigs");
+        }
+    }
+}
---
+++ Floofbot/Services/Repository/Models/WelcomeGate.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Floofbot.Services.Repository.Models
+{
+    public partial class WelcomeGate
+    {
+        [Key]
+        public ulong GuildID { get; set; }
+        public ulong? RoleId { get; set; }
+        public bool Toggle { get; set; }
+    }
+}
+
